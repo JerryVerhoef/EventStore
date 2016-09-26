9ee36353e1b1068046df31215496836f1d0b210a
HayleyC <hayleyjeancampbell@gmail.com>
2016-09-26 09:18:59 +0200

    Wait for emitted events.

--- src/EventStore.Projections.Core.Tests/Services/emitted_streams_tracker/when_tracking/with_tracking_enabled.cs
+++ src/EventStore.Projections.Core.Tests/Services/emitted_streams_tracker/when_tracking/with_tracking_enabled.cs
@@ -2,19 +2,28 @@
 using EventStore.Projections.Core.Services.Processing;
 using NUnit.Framework;
 using System;
+using System.Threading;
 
 namespace EventStore.Projections.Core.Tests.Services.emitted_streams_tracker.when_tracking
 {
     [TestFixture]
     public class with_tracking_enabled : SpecificationWithEmittedStreamsTrackerAndDeleter
     {
+        private ManualResetEvent _eventAppeared = new ManualResetEvent(false);
+        private EventStore.ClientAPI.SystemData.UserCredentials _credentials;
+
         protected override void Given()
         {
+            _credentials = new EventStore.ClientAPI.SystemData.UserCredentials("admin", "changeit");
             base.Given();
         }
 
         protected override void When()
         {
+            _conn.SubscribeToStreamAsync(_projectionNamesBuilder.GetEmittedStreamsName(), true, (s, evnt) => {
+                _eventAppeared.Set();
+            }, userCredentials: _credentials);
+
             _emittedStreamsTracker.TrackEmittedStream(new EmittedEvent[]
             {
                 new EmittedDataEvent(
@@ -26,7 +35,12 @@ namespace EventStore.Projections.Core.Tests.Services.emitted_streams_tracker.whe
         [Test]
         public void should_write_a_stream_tracked_event()
         {
-            var result = _conn.ReadStreamEventsForwardAsync(_projectionNamesBuilder.GetEmittedStreamsName(), 0, 200, false, new EventStore.ClientAPI.SystemData.UserCredentials("admin", "changeit")).Result;
+            if(!_eventAppeared.WaitOne(TimeSpan.FromSeconds(5))) 
+            {
+                Assert.Fail("Timed out waiting for emitted stream event");
+            }
+
+            var result = _conn.ReadStreamEventsForwardAsync(_projectionNamesBuilder.GetEmittedStreamsName(), 0, 200, false, _credentials).Result;
             Assert.AreEqual(1, result.Events.Length);
             Assert.AreEqual("test_stream", Helper.UTF8NoBom.GetString(result.Events[0].Event.Data));
         }
